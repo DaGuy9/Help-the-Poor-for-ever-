6b923e0d540728b3633f683961cbb00f128444ac
DaGuy9 <132631119+DaGuy9@users.noreply.github.com>
2023-12-30 03:30:12 +0100

    Update send_location.php

--- send_location.php
+++ send_location.php
@@ -1,19 +1,28 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $latitude = $_POST['latitude'];
-    $longitude = $_POST['longitude'];
-
-    // Email details
-    $to = "testimony1201@gmail.com";
-    $subject = "User Location";
-    $message = "Latitude: $latitude\nLongitude: $longitude";
-    $headers = "From: testimony1201@gmail.com "; // Change this to your email
-
-    // Send email
-    if (mail($to, $subject, $message, $headers)) {
-        echo "<h2>Location sent successfully!</h2>";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Process the POST request
+    $latitude = $_POST['latitude'] ?? null;
+    $longitude = $_POST['longitude'] ?? null;
+    
+    // Example: Check if latitude and longitude exist, then send an email
+    if ($latitude !== null && $longitude !== null) {
+        // Construct the email message
+        $to = "testimony1201@gmail.com"; // Update this with your email address
+        $subject = "Received Location Data";
+        $message = "Latitude: " . $latitude . "\nLongitude: " . $longitude;
+        
+        // Send the email
+        if (mail($to, $subject, $message)) {
+            echo "Email sent successfully!";
+        } else {
+            echo "Failed to send email.";
+        }
     } else {
-        echo "<h2>Failed to send location. Please try again.</h2>";
+        echo "Latitude or longitude data is missing.";
     }
+} else {
+    header("HTTP/1.1 405 Method Not Allowed");
+    echo "Only POST requests are allowed.";
+    exit;
 }
 ?>
