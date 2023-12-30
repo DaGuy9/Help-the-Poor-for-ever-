03b4912890efe8d38e576f7a3729d54b38458ba2
DaGuy9 <132631119+DaGuy9@users.noreply.github.com>
2023-12-30 01:49:59 +0100

    Create send_location.php
    Php server

---
+++ send_location.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $latitude = $_POST['latitude'];
+    $longitude = $_POST['longitude'];
+
+    // Email details
+    $to = "testimony1201@gmail.com";
+    $subject = "User Location";
+    $message = "Latitude: $latitude\nLongitude: $longitude";
+    $headers = "From: webmaster@example.com"; // Change this to your email
+
+    // Send email
+    if (mail($to, $subject, $message, $headers)) {
+        echo "<h2>Location sent successfully!</h2>";
+    } else {
+        echo "<h2>Failed to send location. Please try again.</h2>";
+    }
+}
+?>
